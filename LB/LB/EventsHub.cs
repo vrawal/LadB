6470cbf12300d680947689f1eea03eb2dad9e72c
vrawal <rawal_vishal@yahoo.com>
2015-07-06 20:51:07 +1000

    First commit

---
+++ LB/LB/EventsHub.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace LB
+{
+    public class EventsHub : Hub
+    {
+        public void Send(string name, string message)
+        {          
+            Clients.All.broadcastMessage(name, message);
+        }
+    }
+}
---
+++ LB/LB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+[assembly: OwinStartup(typeof(LB.Startup))]
+namespace LB
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+    }
+}
